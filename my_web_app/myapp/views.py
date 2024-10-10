bd42e8eddbb36cd145a649365434ef54295a8af3
janisfs <janisfs@mail.ru>
2024-10-11 00:00:32 +0300

    add new code

--- my_web_app/myapp/views.py
+++ my_web_app/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def new(request):
+    return render(request, 'myapp/new.html')
